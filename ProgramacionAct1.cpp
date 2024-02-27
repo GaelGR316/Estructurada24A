38d0aefe5e5741dae32f43e30c1a7360959af69b
Gael Gómez <gaelgr316@gmail.com>
2024-02-27 00:38:18 -0600

    Add files via upload

---
+++ ProgramacionAct1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    printf("*********************************************\n");
+    std::string nombre = "Gael Gomez Rivera";
+    int edad = 19;
+    std::string LugarDeResidencia = "Tepatitlan";
+    std::string fecha = "29/01/2024";
+    std::string carrera = "Ingenieria en Computacion";
+    int semestre = 2;
+
+    std::cout << "Nombre: " << nombre << "\n";
+    std::cout << "Edad: " << edad << "\n";
+    std::cout << "Lugar de Residencia: " << LugarDeResidencia << "\n";
+    std::cout << "Fecha: " << fecha << "\n";
+    std::cout << "Carrera: " << carrera << "\n";
+    std::cout << "Semestre: " << semestre << "\n";
+    
+	printf("*********************************************");
+
+    return 0;
+}
